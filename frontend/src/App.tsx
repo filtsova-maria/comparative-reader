02fdb1689ef2356bcd473f0fbf8edf20b06e1cb1
maria.filtsova <maria.filtsova@netrex.cz>
2025-01-11 22:06:39 +0100

    feat: main styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,48 @@
-import type { Component } from "solid-js";
+import { createSignal, Show, type Component } from "solid-js";
 
-const UploadDocument = () => {
+interface FileInputProps {}
+
+const Document: Component<FileInputProps> = ({ onFileChange }) => {
+  const [file, setFile] = createSignal<File | null>(null);
+  async function uploadFile(formData) {
+    try {
+      const response = await fetch("/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        // document.getElementById('uploadStatus').textContent = 'File uploaded successfully!';
+      } else {
+        // document.getElementById('uploadStatus').textContent = 'Upload failed.';
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      // document.getElementById('uploadStatus').textContent = 'An error occurred while uploading the file.';
+    }
+  }
   return (
-    <div>
-      <input type="file" />
-    </div>
+    <Show
+      when={file() !== null}
+      fallback={
+        <div class="flex">
+          <input
+            type="file"
+            onChange={(e) => setFile(e.target.files?.[0] ?? null)}
+          />
+        </div>
+      }
+    >
+      file
+    </Show>
   );
 };
 
 const App: Component = () => {
   return (
-    <div class="text-lg font-thin bg-green-500">
-      <UploadDocument />
-      <UploadDocument />
+    <div class="grid grid-cols-2 h-screen items-center justify-items-center">
+      <Document />
+      <Document />
     </div>
   );
 };
