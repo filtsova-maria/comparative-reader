7b5362165b78e0fbf41d000b298025a15143a154
maria.filtsova <maria.filtsova@netrex.cz>
2025-06-28 17:30:39 +0200

    feat: add all ui components and basic styles, create components folder

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as IconButton } from "./IconButton";
+export { default as Document } from "./Document";
+export { default as Tooltip } from "./Tooltip";
+export { default as Toolbar } from "./Toolbar";
