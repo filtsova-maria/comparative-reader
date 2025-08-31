63f4af0319b843221bd3fecf80148b20df065edb
Maria Filtsova <filtsovamaria@seznam.cz>
2025-08-31 16:47:38 +0200

    feat: implement basic segment selection

--- frontend/src/components/Document/utils.ts
+++ frontend/src/components/Document/utils.ts
@@ -1,3 +1,5 @@
+import { TDocumentType } from "../../store/DocumentStore";
+
 export const scrollToSegment = (id: string) => {
   document
     .getElementById(id)
@@ -6,4 +8,15 @@ export const scrollToSegment = (id: string) => {
 
 export const splitIntoSentences = (text: string): string[] => {
   return text.match(/[^\.!\?]+[\.!\?]+/g) || [];
+};
+
+export const getSegmentIdByIndex = (
+  type: TDocumentType,
+  index: number,
+): string => {
+  return `${type}-segment-${index}`;
+};
+
+export const getSegmentIndexById = (id: string): number => {
+  return parseInt(id.split("-").pop() || "-1", 10);
 };
